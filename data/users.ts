0a11a5b399182a1b89d1bdbaa7c2a6700d58cd24
Sebastian <139753003+Iakobson@users.noreply.github.com>
2023-11-27 18:43:20 +0200

    Add files via upload

---
+++ data/users.ts
@@ -0,0 +1,30 @@
+// @file: data/users.ts
+export const users = [
+    {
+      id: "101",
+      email: "robot@gmail.com",
+      name: "Robot",
+      password: "werter1985",
+      role: "admin",
+      image: "/cat-werter.jpg",
+      alt: "cat-werter"
+    },
+    {
+      id: "102",
+      email: "barbarella@gmail.com",
+      name: "Barbarella",
+      password: "space1968",
+      role: "guest",
+      image: "/cat-barbarella.jpg",
+      alt: "cat-barbarella"
+    },
+    {
+      id: "103",
+      email: "tracker@gmail.com",
+      name: "Tracker",
+      password: "busik1999",
+      role: "guest",
+      image: "/animal-racker.jpg",
+      alt: "animal-racker"
+    },
+  ];
